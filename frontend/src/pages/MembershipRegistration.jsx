1cd17f42aa1a31c2cadc5e6b6d77e7ef4853a985
Manya <manya111203@gmail.com>
2025-12-19 17:52:04 +0530

    update: remove mode: no-cors

--- frontend/src/pages/MembershipRegistration.jsx
+++ frontend/src/pages/MembershipRegistration.jsx
@@ -175,7 +175,7 @@ const MembershipRegistration = () => {
     try {
       await fetch(GOOGLE_SCRIPT_URL, {
         method: "POST",
-        mode: "no-cors",
+        // mode: "no-cors",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
